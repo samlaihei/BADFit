eab1322044d8c9940274ea29c6d130e5947546ab
samlaihei <82487191+samlaihei@users.noreply.github.com>
2023-03-05 13:41:12 +1100

    Update example.py

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ https://heasarc.gsfc.nasa.gov/xanadu/xspec/
 Sherpa Installation:
 https://sherpa.readthedocs.io/en/latest/install.html
 
+Source install with conda:
+https://sherpa.readthedocs.io/en/latest/developer/index.html#source-install-with-conda
+
 Run example.py after installation and use it as a guide.
--- example.py
+++ example.py
@@ -23,8 +23,8 @@ if False:
 else:
 	freq, power, epower = [], [], []
 
-#J2157 = BADFit.BADFit('example', 'SLIMBH', lams, flux, eflux, redshift, freq=freq, power=power, epower=epower, ra=329.36758, dec=-36.03752)
-J2157 = BADFit.BADFit('example', 'SLIMBH', lams, flux, eflux, redshift, ra=329.36758, dec=-36.03752)
+#J2157 = BADFit.BADFit('example', 'KERRBB', lams, flux, eflux, redshift, freq=freq, power=power, epower=epower, ra=329.36758, dec=-36.03752)
+J2157 = BADFit.BADFit('example', 'SLIMBH', lams, flux, eflux, redshift)
 #J2157.createPlotFromFile()
 J2157.runMCMC()
 
