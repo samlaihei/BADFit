50d10d7571ec6a8e1a8d2be020e03b062979ed69
samlaihei <82487191+samlaihei@users.noreply.github.com>
2023-04-21 18:02:19 +1000

    Update BADFit.py

--- BADFit.py
+++ BADFit.py
@@ -660,7 +660,7 @@ class BADFit():
 			fit_ax = fig.add_subplot(num_params, num_params, (fit_plot[0], fit_plot[-1]))
 
 		for ind, ax in enumerate(hist_axs):
-			current_dist = np.transpose(flattened_mcmc_chain)[ind]
+			current_dist = np.array(np.transpose(flattened_mcmc_chain)[ind])
 			sns.histplot(current_dist, kde='False', ax = ax, color='C0', fill=True, element="step")
 			ax.tick_params(axis='both', which='both', direction='in', left=False, labelleft=False, labelbottom=False)
 			ax.spines['top'].set_visible(False)
